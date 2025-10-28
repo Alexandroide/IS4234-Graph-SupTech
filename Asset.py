6cc5825fc21e47adfc930126805fb42e6b96e398
Alexandre <65114422+Alexandroide@users.noreply.github.com>
2025-10-28 16:54:08 +0800

    Update Asset.py, add metrics to compute operational reliance -> edge weight

--- Asset.py
+++ Asset.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import datetime
 import hashlib
 import re
 
@@ -8,18 +8,37 @@ class Asset:
         company_id: str,
         supplier_id: str,
         asset_name: str,
+        asset_type: str,
         purchase_date: str,
-        deployment_date: str
+        deployment_date: str,
+        # reporting_period: str = None,
+        # Reported metrics
+        revenue_share: float = None,
+        critical_service_share: float = None,
+        client_share: float = None,
+        capacity_share: float = None,
+        redundancy_level: float = None
     ):
-        
+        # Basic identifiers
         self.asset_id = self.compute_asset_id(supplier_id, asset_name)  # global asset hash
         self.company_asset_id = self.compute_company_asset_id(company_id, self.asset_id)  # unique per company
-
         self.asset_name = asset_name
-        self.supplier_id = supplier_id
+        self.asset_type = asset_type
         self.company_id = company_id
+        self.supplier_id = supplier_id
         self.purchase_date = purchase_date
         self.deployment_date = deployment_date
+
+        # Reported metrics
+        self.revenue_share = revenue_share
+        self.critical_service_share = critical_service_share
+        self.client_share = client_share
+        self.capacity_share = capacity_share
+        self.redundancy_level = redundancy_level
+
+        # Computed metrics
+        self.operational_reliance = None
+        self.timestamp = datetime.datetime.utcnow().isoformat()
 
     # Static methods
     @staticmethod
@@ -42,3 +61,8 @@ class Asset:
         """Generate a unique ID for this asset owned by the company."""
         hash_input = f"{company_id}-{asset_id}".encode('utf-8')
         return hashlib.sha256(hash_input).hexdigest()[:16]
+    
+    # Operational reliance computation
+    def compute_operational_reliance(self) -> float:
+        ### TBD
+        return None
