ee7b06763079043dcc7f57c2de82ccbcc713a801
Alexandre <65114422+Alexandroide@users.noreply.github.com>
2025-10-29 00:12:49 +0800

    Update update_company_data.py

--- src/update_company_data.py
+++ src/update_company_data.py
@@ -3,7 +3,7 @@ import json
 import pandas as pd
 from company import Company  # adjust path according to your repo structure
 
-CSV_FILE = "../data/test_C1_data.csv"
+CSV_FILE = "../data/test/test_C1_data.csv"
 DB_FILE = "../data/company_data.json"
 
 # Step 1: Read CSV
